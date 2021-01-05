6de86e879d59b2fb888d40e1652d018ed6967311
Virginia Cruz <virginiarcruz@gmail.com>
2021-01-04 23:16:00 -0300

    building menu full on desktop and mobile

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,36 @@
+import { screen, fireEvent } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o menufull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verficar se o menu está hidden
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar para abrir o menu e checar se está aberto
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar para fechar e checar se está fechado
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+})
