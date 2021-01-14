ccb4d5d52061cb725123a2566cee34c32b456cd4
Virginia Cruz <virginiarcruz@gmail.com>
2021-01-13 22:00:55 -0300

    finishi home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="The best project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
